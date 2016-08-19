96c98eac64c55bad586143e11fb43cc51c8bfee5
ZG <grubisic.zvonimir@gmail.com>
2016-08-19 23:25:12 +0200

    added Visual studio project and testcontroller

---
+++ DemonstratureAPI/DemonstratureAPI/APIControllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemonstratureAPI.APIControllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("api/test")]
+        public IHttpActionResult TestValues([FromUri]string x)
+        {
+            if (x != "")
+            {
+                return Ok(x);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
