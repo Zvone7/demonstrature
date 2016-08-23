c9851e95528143994856853cd20760b284c37fe5
ZG <grubisic.zvonimir@gmail.com>
2016-08-23 21:24:31 +0200

    x

---
+++ DemonstratureAPI/DemonstratureAPI/APIControllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using DemonstratureBLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DemonstratureCM.DTO;
+
+namespace DemonstratureAPI.APIControllers
+{
+    public class UserController : ApiController
+    {
+        [HttpGet]
+        [Route("api/user/all")]
+        public IHttpActionResult GetAllUsers()
+        {
+            var instance = new UserLogic();
+            var result = instance.GetAllUsersBLL();
+            if (result!=null) { return Ok(result); }
+            else { return BadRequest(); }
+        }
+    }
+}
---
+++ DemonstratureAPI/DemonstratureBLL/UserLogic.cs
@@ -0,0 +1,31 @@
+﻿using DemonstratureCM.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemonstratureBLL
+{   public class UserLogic
+    {
+        List<MyUserDTO> allUsers = new List<MyUserDTO>();
+        int _userCount = 10;
+        public UserLogic()
+        {
+            for (int i = 0; i < _userCount; i++)
+            {
+                var newUser = new MyUserDTO();
+                newUser.UserName = "iprezime" + (i + 1).ToString();
+                newUser.FullName = "ime" + (i + 1).ToString() + " prezime";
+                newUser.ID = i;
+                if (i % 5 == 0) { newUser.Role = "Assistant"; }
+                else { newUser.Role = "Student"; }
+                allUsers.Add(newUser);
+            }
+        }
+        public List<MyUserDTO> GetAllUsersBLL()
+        {           
+            return allUsers;
+        }
+    }
+}
