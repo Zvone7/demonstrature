849b87ab93d7831cfa8f914fb663f1707134be90
ZG <grubisic.zvonimir@gmail.com>
2018-06-18 17:57:52 +0200

    model fix

---
+++ DemonstratureCM/DTO/MyUserDTO.cs
@@ -0,0 +1,14 @@
+﻿using DemonstratureCM.BM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemonstratureCM.DTO
+{
+	public class MyUserDto : MyUserBm
+	{
+		public string Password { get; set; }
+	}
+}
