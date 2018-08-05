ecc5a7d6998f726e3dfb602163865d2f8ddb54a6
ZG <grubisic.zvonimir@gmail.com>
2018-08-05 13:38:36 +0200

    constant fix

--- DemonstratureCM/Settings/GlobalAppSettings.cs
+++ DemonstratureCM/Settings/GlobalAppSettings.cs
@@ -15,7 +15,7 @@ namespace DemonstratureCM.Settings
         public const int NumRow = 4;
         public const int DayForDeadline = 6; // 0 -> 6 | sunday -> saturday
         public const string NoDateString = "Nema podataka0:00:00";
-        public const string RoleAdministrator = "A";
-        public const string RoleDemonstrator = "A";
+        public const string RoleAdministrator = "Administrator";
+        public const string RoleDemonstrator = "Demonstrator";
     }
 }
